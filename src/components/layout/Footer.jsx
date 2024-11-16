06a25efe1ff21c5e3b8365adedde0e757d4ba091
Nelen Maharjan <132544797+nelen-maharjan@users.noreply.github.com>
2024-11-16 11:16:55 +0545

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -5,7 +5,7 @@ import { FaFacebookF, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/f
 const Footer = () => {
   return (
     <footer className='border-t px-6 py-4 text-gray-500 mt-16 bg-gray-100'>
-      <div className='flex justify-between gap-8'>
+      <div className='flex justify-between items-center gap-8'>
         <div className=''>
           <iframe className='w-64 h-40 border-0' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d262.6351071876167!2d85.27504197017744!3d27.668379123160964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb180628d596cf%3A0x51e051805c5e5072!2sPanga%20Health%20Center%2C%20Panga%20%2CKirtipur!5e0!3m2!1sen!2snp!4v1726319257012!5m2!1sen!2snp" width="400" height="300" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
